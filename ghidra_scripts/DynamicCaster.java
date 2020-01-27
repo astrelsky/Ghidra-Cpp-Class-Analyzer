2972cc7f09b154b94f438afa5d0a61e725527bb9
astrelsky <ajs222@njit.edu>
2020-01-27 14:53:04 -0500

    Finished DynamicCaster script

--- ghidra_scripts/DynamicCaster.java
+++ ghidra_scripts/DynamicCaster.java
@@ -1,142 +1,259 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019-2020 Andrew J. Strelsky
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+// Finds all calls to __dynamic_cast, determines the source and destination data types from the
+// __class_type_info parameters and then generated a function signature override. This is extremely
+// useful as it assists the decompiler's type propogation algorithm which cannot handle virtual classes.
 //@category CppClassAnalyzer
+//@author Andrew J. Strelsky
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import ghidra.app.cmd.data.rtti.ClassTypeInfo;
-import ghidra.app.cmd.data.rtti.gcc.ClassTypeInfoUtils;
-import ghidra.app.cmd.data.rtti.gcc.GnuUtils;
 import ghidra.app.cmd.data.rtti.gcc.TypeInfoUtils;
 import ghidra.app.cmd.data.rtti.gcc.factory.TypeInfoFactory;
-import ghidra.app.plugin.core.analysis.ConstantPropagationContextEvaluator;
+import ghidra.app.plugin.core.analysis.AutoAnalysisManager;
+import ghidra.app.plugin.core.analysis.ConstantPropagationAnalyzer;
 import ghidra.app.script.GhidraScript;
 import ghidra.program.model.address.Address;
-import ghidra.program.model.address.AddressRange;
-import ghidra.program.model.address.AddressRangeImpl;
-import ghidra.program.model.data.DataType;
-import ghidra.program.model.data.DataTypeManager;
-import ghidra.program.model.data.DataUtilities;
-import ghidra.program.model.data.Pointer;
-import ghidra.program.model.data.Undefined;
-import ghidra.program.model.data.DataUtilities.ClearDataMode;
+import ghidra.program.model.data.*;
 import ghidra.program.model.lang.Register;
-import ghidra.program.model.listing.Data;
 import ghidra.program.model.listing.Function;
+import ghidra.program.model.listing.FunctionSignature;
 import ghidra.program.model.listing.Instruction;
 import ghidra.program.model.listing.Parameter;
-import ghidra.program.model.listing.ProgramContext;
+import ghidra.program.model.listing.Program;
+import ghidra.program.model.pcode.DataTypeSymbol;
+import ghidra.program.model.pcode.HighFunction;
+import ghidra.program.model.symbol.Namespace;
 import ghidra.program.model.symbol.RefType;
 import ghidra.program.model.symbol.Reference;
+import ghidra.program.model.symbol.ReferenceManager;
+import ghidra.program.model.symbol.SymbolTable;
 import ghidra.program.util.SymbolicPropogator;
-import ghidra.program.util.VarnodeContext;
+import ghidra.program.util.SymbolicPropogator.Value;
+import ghidra.util.classfinder.ClassSearcher;
+import ghidra.util.exception.CancelledException;
+import ghidra.util.task.TaskMonitor;
 
 public class DynamicCaster extends GhidraScript {
 
-    private SymbolicPropogator symProp;
-    private Parameter srcPointer;
-    private Parameter srcType;
+	private static final String UNSUPPORTED_MESSAGE =
+		"Currently only processors passing parameters via registers are supported.";
+	private static final String FORMAL_SIGNATURE =
+		"void * __dynamic_cast(void * src_ptr, __class_type_info * src_type, __class_type_info * dst_type, ptrdiff_t src2dst)";
+	private static final String DYNAMIC_CAST = "__dynamic_cast";
+	private static final String TMP_NAME = "tmpname";
+	private static final String NAME_ROOT = "prt";
+	private static final String AUTO_CAT = "/auto_proto";
+
+	private Register srcReg;
+	private Register destReg;
+	private SymbolTable table;
+	private DataTypeManager dtm;
+	private FunctionSignature sig;
+	private ConstantPropagationAnalyzer analyzer;
 
     @Override
     public void run() throws Exception {
-        println("This script is currently in development and may not function properly");
-        List<Function> functions = getGlobalFunctions("__dynamic_cast");
-        if (functions.size() > 1) {
-            println("More than one __dynamic_cast function found.");
-            return;
-        }
-        if (functions.isEmpty()) {
-            println("__dynamic_cast function not found");
-            return;
-        }
-        Function dynamicCast = functions.get(0);
-        Parameter[] parameters = dynamicCast.getParameters();
+		table = currentProgram.getSymbolTable();
+		dtm = currentProgram.getDataTypeManager();
+		analyzer = getConstantAnalyzer(currentProgram);
+		final Function dynamicCast = getDynamicCast();
+		if (dynamicCast == null) {
+			return;
+		}
+		if (!dynamicCast.getPrototypeString(true, false).equals(FORMAL_SIGNATURE)) {
+			printerr("The function at "+dynamicCast.getEntryPoint().toString()
+					 +" doesnt match the cxxabi defined functions signature:\n"
+					 +FORMAL_SIGNATURE);
+			return;
+		}
+		sig = dynamicCast.getSignature();
+        final Parameter[] parameters = dynamicCast.getParameters();
         if (parameters.length != 4) {
-            println("Unexpected number of __dynamic_cast parameters");
+            printerr("Unexpected number of __dynamic_cast parameters");
             return;
         }
-        srcPointer = parameters[0];
-        srcType = parameters[1];
-        symProp = new SymbolicPropogator(currentProgram);
-        symProp.setParamRefCheck(true);
-        symProp.setReturnRefCheck(true);
-        symProp.setStoredRefCheck(true);
-        Reference[] references = getReferencesTo(dynamicCast.getEntryPoint());
-        monitor.initialize(references.length);
+        if (!parameters[2].isRegisterVariable()) {
+			printerr(UNSUPPORTED_MESSAGE);
+			return;
+		}
+		srcReg = parameters[1].getRegister();
+		destReg = parameters[2].getRegister();
+		final ReferenceManager manager = currentProgram.getReferenceManager();
+		final List<Address> addresses =
+			Arrays.stream(getReferencesTo(dynamicCast.getEntryPoint()))
+				  .filter(DynamicCaster::isCall)
+				  .map(Reference::getFromAddress)
+				  .filter(Predicate.not(manager::hasReferencesTo))
+				  .collect(Collectors.toList());
+        monitor.initialize(addresses.size());
         monitor.setMessage("Analyzing __dynamic_cast calls");
-        for (Reference reference : references) {
+        for (Address address : addresses) {
             monitor.checkCanceled();
-            if (!reference.getReferenceType().isFlow()) {
-                monitor.incrementProgress(1);
-                continue;
-            }
-            ConstantPropagationContextEvaluator eval =
-                new ConstantPropagationContextEvaluator(true);
-            Function function = getFunctionContaining(reference.getFromAddress());
-            Instruction inst = getInstructionAt(reference.getFromAddress());
-            int delaySlotDepth = inst.getDelaySlotDepth();
-            for (int i = 0; i < delaySlotDepth; i++) {
-                inst = inst.getNext();
-            }
-            symProp.flowConstants(
-                function.getEntryPoint(), function.getBody(), eval, true, monitor);
-            doDynamicCast(reference.getFromAddress());
+            doDynamicCast(address);
             monitor.incrementProgress(1);
         }
-    }
+	}
+
+	private Function getDynamicCast() {
+		List<Function> functions = getGlobalFunctions(DYNAMIC_CAST);
+        if (functions.size() > 1) {
+            printerr("More than one __dynamic_cast function found.");
+            return null;
+        }
+        if (functions.isEmpty()) {
+            printerr("__dynamic_cast function not found");
+            return null;
+        }
+		return functions.get(0);
+	}
+
+	private static boolean isCall(Reference r) {
+		final RefType type = r.getReferenceType();
+		if (type.isCall()) {
+			return !(type.isComputed() || type.isIndirect());
+		}
+		return false;
+	}
+
+	private Address getDelayAddress(Address address) {
+		Instruction inst = getInstructionAt(address);
+		int depth = inst.getDelaySlotDepth();
+		if (depth > 0) {
+			while (depth >= 0) {
+				inst = inst.getNext();
+				depth--;
+			}
+		}
+		return inst.getAddress();
+	}
+
+	private ClassTypeInfo getType(SymbolicPropogator prop, Address address, Register reg) {
+		Value value = prop.getRegisterValue(address, reg);
+		if (value != null) {
+			final Address typeAddress = toAddr(value.getValue());
+			if (TypeInfoUtils.isTypeInfo(currentProgram, typeAddress)) {
+				return (ClassTypeInfo) TypeInfoFactory.getTypeInfo(
+					currentProgram, typeAddress);
+			}
+		}
+		value = prop.getRegisterValue(getDelayAddress(address), reg);
+		if (value != null) {
+			final Address typeAddress = toAddr(value.getValue());
+			if (TypeInfoUtils.isTypeInfo(currentProgram, typeAddress)) {
+				return (ClassTypeInfo) TypeInfoFactory.getTypeInfo(
+					currentProgram, typeAddress);
+			}
+		}
+		return null;
+	}
 
     private void doDynamicCast(Address address) throws Exception {
-        SymbolicPropogator.Value srcValue = symProp.getRegisterValue(
-            address, srcPointer.getRegister());
-        SymbolicPropogator.Value srcTypeValue = symProp.getRegisterValue(
-            address, srcType.getRegister());
-        Address dataAddress = null;
-        Function function = getFunctionContaining(address);
-        if (srcValue == null) {
-            AddressRange range = new AddressRangeImpl(function.getEntryPoint(), address);
-            Instruction inst = getInstructionAt(function.getEntryPoint());
-            while (range.contains(inst.getAddress())) {
-                for (Reference reference : inst.getReferencesFrom()) {
-                    if (reference.getReferenceType().isData()) {
-                        for (Object object : inst.getResultObjects()) {
-                            if (object instanceof Register) {
-                                Register reg = ((Register) object).getBaseRegister();
-                                if (reg.contains(srcPointer.getRegister())) {
-                                    dataAddress = reference.getToAddress();
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                inst = inst.getNext();
-            }
-        } else {
-            dataAddress = toAddr(srcValue.getValue());
-        }
-        if (dataAddress != null && srcTypeValue != null) {
-            Address typeAddress = toAddr(srcTypeValue.getValue());
-            if (TypeInfoUtils.isTypeInfo(currentProgram, typeAddress)) {
-                ClassTypeInfo type = (ClassTypeInfo) TypeInfoFactory.getTypeInfo(
-                    currentProgram, typeAddress);
-                DataTypeManager dtm = currentProgram.getDataTypeManager();
-                DataType dt = srcValue == null ? dtm.getPointer(type.getClassDataType())
-                    : type.getClassDataType();
-                if (isValidDataAddress(dataAddress)) {
-                    Data data = getDataAt(dataAddress);
-                    if (data == null || Undefined.isUndefined(data.getDataType())) {
-                        println("created "+dt.toString()+" at "+dataAddress.toString()+" from "+address.toString());
-                        doCreateData(dataAddress, dt);
-                    }
-                }
-            }
-        }
-    }
+		final Function function = getFunctionContaining(address);
+		final SymbolicPropogator prop = analyzeFunction(function, analyzer, monitor);
+		final ClassTypeInfo srcType = getType(prop, address, srcReg);
+		final ClassTypeInfo destType = getType(prop, address, destReg);
+		if (srcType != null && destType != null) {
+			overrideFunction(function, address, srcType, destType);
+		}
+	}
 
-    private boolean isValidDataAddress(Address address) {
-        return GnuUtils.isDataBlock(getMemoryBlock(address));
-    }
+	private static ParameterDefinition getParameter(DataType dataType) {
+		final DataType dt = PointerDataType.getPointer(dataType, -1);
+		return new ParameterDefinitionImpl(null, dt, null);
+	}
 
-    private Data doCreateData(Address address, DataType dt) throws Exception {
-        return DataUtilities.createData(currentProgram, address, dt,
-                                 currentProgram.getDefaultPointerSize(), false,
-                                 ClearDataMode.CLEAR_ALL_CONFLICT_DATA);
+	private FunctionDefinition getFunctionSignature(ClassTypeInfo src, ClassTypeInfo dest,
+		Function function)
+			throws Exception {
+				final FunctionDefinition def = new FunctionDefinitionDataType(sig);
+				final DataType destType = PointerDataType.getPointer(dest.getClassDataType(), -1);
+				final ParameterDefinition[] params = def.getArguments();
+				params[0] = getParameter(src.getClassDataType());
+				params[1] = getParameter(src.getDataType());
+				params[2] = getParameter(dest.getDataType());
+				def.setName(TMP_NAME);
+				def.setArguments(params);
+				def.setReturnType(destType);
+				return def;
     }
+	
+	private void overrideFunction(Function function, Address address,
+		ClassTypeInfo src, ClassTypeInfo dest) throws Exception {
+            int transaction = -1; 
+            if (currentProgram.getCurrentTransaction() == null) {
+                transaction = currentProgram.startTransaction("Override Signature");
+            }
+            boolean commit = false;
+            try {
+				final FunctionDefinition def = getFunctionSignature(src, dest, function);
+				if (def != null) {
+					DataTypeSymbol symbol = new DataTypeSymbol(def, NAME_ROOT, AUTO_CAT);
+					Namespace space = HighFunction.findCreateOverrideSpace(function);
+					if (space != null) {
+						symbol.writeSymbol(table, address, space, dtm, true);
+						commit = true;
+					}
+				}
+            }
+            catch (Exception e) {
+				printerr("Error overriding signature: " + e.getMessage());
+            }
+            finally {
+                if (transaction != -1) {
+                    currentProgram.endTransaction(transaction, commit);
+                }
+            }
+	}
+
+	    // These should be in a Util class somewhere!
+
+		private static ConstantPropagationAnalyzer getConstantAnalyzer(Program program) {
+			AutoAnalysisManager mgr = AutoAnalysisManager.getAnalysisManager(program);
+			List<ConstantPropagationAnalyzer> analyzers = 
+				ClassSearcher.getInstances(ConstantPropagationAnalyzer.class);
+			for (ConstantPropagationAnalyzer analyzer : analyzers) {
+				if (analyzer.canAnalyze(program)) {
+					return (ConstantPropagationAnalyzer) mgr.getAnalyzer(analyzer.getName());
+				}
+			}
+			return null;
+		}
+	
+		public static SymbolicPropogator analyzeFunction(Function function,
+			ConstantPropagationAnalyzer analyzer, TaskMonitor monitor) throws CancelledException {
+				Program program = function.getProgram();
+				SymbolicPropogator symEval = new SymbolicPropogator(program);
+				symEval.setParamRefCheck(true);
+				symEval.setReturnRefCheck(true);
+				symEval.setStoredRefCheck(true);
+				analyzer.flowConstants(program, function.getEntryPoint(), function.getBody(),
+									   symEval, monitor);
+				return symEval;
+		}
 }
