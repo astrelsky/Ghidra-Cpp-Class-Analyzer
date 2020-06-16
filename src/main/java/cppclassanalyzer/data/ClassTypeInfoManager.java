616cdafceec6000292f8413ceb6bfd19381a65cc
astrelsky <ajs222@njit.edu>
2020-06-16 11:34:07 -0400

    ClassTypeInfoManager documentation

--- src/main/java/cppclassanalyzer/data/ClassTypeInfoManager.java
+++ src/main/java/cppclassanalyzer/data/ClassTypeInfoManager.java
@@ -16,25 +16,104 @@ import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.GhidraClass;
 import ghidra.program.model.symbol.Namespace;
 
+/**
+ * ClassTypeInfoManager manages all {@link ClassTypeInfo} within
+ * a {@link ghidra.framework.model.DomainObject DomainObject}
+ */
 public interface ClassTypeInfoManager extends ErrorHandler {
 
+	/** The value which represents an invalid database key */
 	public static final long INVALID_KEY = AddressMap.INVALID_ADDRESS_KEY;
 
+	/**
+	 * Gets the name of this manager
+	 * @return the managers name
+	 */
 	String getName();
 
+	/**
+	 * Gets the icon for this manager to be used in the
+	 * {@link ghidra.app.plugin.prototype.typemgr.TypeInfoArchiveGTree TypeInfoArchiveGTree}
+	 * @param expanded
+	 * @return the icon to be used in the tree
+	 */
 	Icon getIcon(boolean expanded);
 
+	/**
+	 * Returns a ClassTypeInfo that is managed by this ClassTypeInfoManager.
+	 * If one does not currently exist a new one is created.
+	 * @param type the type to resolve
+	 * @return the equivalent type managed by this ClassTypeInfoManager
+	 */
 	ClassTypeInfoDB resolve(ClassTypeInfo type);
+
+	/**
+	 * Gets the ClassTypeInfo for the corresponding database key
+	 * @param key the database key
+	 * @return the ClassTypeInfo or null if it doesn't exist
+	 */
 	ClassTypeInfoDB getType(long key);
+
+	/**
+	 * Gets the ClassTypeInfo for the corresponding {@link GhidraClass}
+	 * @param gc the GhidraClass
+	 * @return the ClassTypeInfo or null if it doesn't exist
+	 * @throws UnresolvedClassTypeInfoException if this type requires a copy relocation
+	 * which cannot be resolved.
+	 */
 	ClassTypeInfoDB getType(GhidraClass gc) throws UnresolvedClassTypeInfoException;
+
+	/**
+	 * Gets the ClassTypeInfo for the corresponding {@link Function}
+	 * @param fun the function
+	 * @return the ClassTypeInfo or null if it doesn't exist
+	 * @throws UnresolvedClassTypeInfoException if this type requires a copy relocation
+	 * which cannot be resolved.
+	 */
 	ClassTypeInfoDB getType(Function fun) throws UnresolvedClassTypeInfoException;
+
+	/**
+	 * Gets the ClassTypeInfo with the specified name and {@link Namespace}
+	 * @param name the type's name
+	 * @param namespace the type's namespace
+	 * @return the ClassTypeInfo or null if it doesn't exist
+	 * @throws UnresolvedClassTypeInfoException if this type requires a copy relocation
+	 * which cannot be resolved.
+	 */
 	ClassTypeInfoDB getType(String name, Namespace namespace)
 		throws UnresolvedClassTypeInfoException;
-	ClassTypeInfoDB getType(String typeName) throws UnresolvedClassTypeInfoException;
 
+	/**
+	 * Gets the ClassTypeInfo with the specified symbol.
+	 * The supplied symbol should be mangled.
+	 * @param symbolName the mangled symbol name
+	 * @return the ClassTypeInfo or null if it doesn't exist
+	 * @throws UnresolvedClassTypeInfoException if this type requires a copy relocation
+	 * which cannot be resolved.
+	 */
+	ClassTypeInfoDB getType(String symbolName) throws UnresolvedClassTypeInfoException;
+
+	/**
+	 * Gets an iterable over all the managed ClassTypeInfos
+	 * @return an iterable over all the managed ClassTypeInfos
+	 */
 	Iterable<ClassTypeInfoDB> getTypes();
+
+	/**
+	 * Gets a stream of all the managed ClassTypeInfos
+	 * @return a stream of all the managed ClassTypeInfos
+	 */
 	Stream<ClassTypeInfoDB> getTypeStream();
+
+	/**
+	 * Gets the number of managed ClassTypeInfos
+	 * @return the number of managed ClassTypeInfos
+	 */
 	int getTypeCount();
 
+	/**
+	 * Gets the {@link TypeInfoTreeNodeManager} for this ClassTypeInfoManager
+	 * @return this manager's TypeInfoTreeNodeManager
+	 */
 	TypeInfoTreeNodeManager getTreeNodeManager();
 }
--- src/main/java/cppclassanalyzer/data/ProgramClassTypeInfoManager.java
+++ src/main/java/cppclassanalyzer/data/ProgramClassTypeInfoManager.java
@@ -2,7 +2,6 @@ package cppclassanalyzer.data;
 
 import java.util.stream.Stream;
 
-import ghidra.app.cmd.data.rtti.ClassTypeInfo;
 import ghidra.app.cmd.data.rtti.TypeInfo;
 import ghidra.app.cmd.data.rtti.Vtable;
 import ghidra.app.cmd.data.rtti.gcc.UnresolvedClassTypeInfoException;
@@ -15,35 +14,136 @@ import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
 
 /**
- * Manager for {@link ClassTypeInfo}
+ * {@link ClassTypeInfoManager} for a {@link Program}
  */
 public interface ProgramClassTypeInfoManager extends TypeInfoManager, ClassTypeInfoManager {
 
+	/**
+	 * Gets the program
+	 * @return the program
+	 */
 	Program getProgram();
+
+	/**
+	 * Gets the ClassTypeInfo at the specified address
+	 * @param address the address of the ClassTypeInfo
+	 * @return the ClassTypeInfo or null if invalid
+	 * @throws UnresolvedClassTypeInfoException if this type requires a copy relocation
+	 * which cannot be resolved.
+	 */
 	ClassTypeInfoDB getType(Address address) throws UnresolvedClassTypeInfoException;
+
+	/**
+	 * Returns a Vtable that is managed by this ProgramClassTypeInfoManager.
+	 * If one does not currently exist a new one is created.
+	 * @param vtable the vtable to resolve
+	 * @return the equivalent vtable managed by this ProgramClassTypeInfoManager
+	 */
 	Vtable resolve(Vtable vtable);
+
+	/**
+	 * Gets the Vtable at the specified address
+	 * @param address the address of the vtable
+	 * @return the vtable at the address or null if none is present
+	 */
 	Vtable getVtable(Address address);
+
+	@Override
 	default Iterable<ClassTypeInfoDB> getTypes() {
 		return getTypes(false);
 	}
+
+	/**
+	 * Gets an iterable over all the managed ClassTypeInfos in the specified direction
+	 * @param reverse true if the types should be provided in reverse
+	 * @return an iterable over all the managed ClassTypeInfos
+	 */
 	Iterable<ClassTypeInfoDB> getTypes(boolean reverse);
+
+	/**
+	 * Gets an iterable over all the managed Vtables
+	 * @return an iterable over all the managed Vtables
+	 */
 	default Iterable<Vtable> getVtables() {
 		return getVtableIterable(false);
 	}
+
+	/**
+	 * Gets an iterable over all the managed Vtables in the specified direction
+	 * @param reverse true if the Vtables should be provided in reverse
+	 * @return an iterable over all the managed Vtables
+	 */
 	Iterable<Vtable> getVtableIterable(boolean reverse);
+
+	/**
+	 * Gets the number of managed Vtables
+	 * @return the number of managed Vtables
+	 */
 	int getVtableCount();
+
+	@Override
 	default Stream<ClassTypeInfoDB> getTypeStream() {
 		return getTypeStream(false);
 	}
+
+	/**
+	 * Gets a stream of all the managed ClassTypeInfos in the specified direction
+	 * @param reverse true if the types should be provided in reverse
+	 * @return a stream of all the managed ClassTypeInfos
+	 */
 	Stream<ClassTypeInfoDB> getTypeStream(boolean reverse);
+
+	/**
+	 * Gets a stream of all the managed Vtables
+	 * @return a stream of all the managed Vtables
+	 */
 	Stream<Vtable> getVtableStream();
+
+	/**
+	 * Gets the external ClassTypeInfo at the specified address.
+	 * The provided address must be the address of a copy relocation or within the
+	 * {@code <EXTERNAL>} AddressSpace. In order for the ClassTypeInfo to be retreived
+	 * a ProjectClassTypeInfoManager containing the symbol at the address must be open
+	 * in the {@link ghidra.app.plugin.prototype.typemgr.node.TypeInfoTreeNodeManager
+	 * TypeInfoTreeNodeManager}
+	 * @param address the address of the external ClassTypeInfo symbol
+	 * @return the ClassTypeInfo at the specified address or null if the data was not found
+	 * @see #getTreeNodeManager
+	 */
 	ClassTypeInfoDB getExternalClassTypeInfo(Address address);
 
+	/**
+	 * Attempts to locate the vtables for all of the managed ClassTypeInfos
+	 * @param monitor the TaskMonitor used to monitor the progress
+	 * @throws CancelledException if the task is cancelled
+	 */
 	void findVtables(TaskMonitor monitor) throws CancelledException;
 
+	/**
+	 * Returns a ClassTypeInfo that is managed by this ClassTypeInfoManager.
+	 * If the ArchivedClassTypeInfo hasn't already been resolved the type
+	 * will be created and all required data will be added to the program.
+	 * @param type the type to resolve
+	 * @return the equivalent type managed by this ClassTypeInfoManager
+	 */
 	ClassTypeInfoDB resolve(ArchivedClassTypeInfo type);
+
+	/**
+	 * Returns a Vtable that is managed by this ClassTypeInfoManager.
+	 * If the ArchivedGnuVtable hasn't already been resolved the vtable
+	 * will be created and all required data will be added to the program.
+	 * @param vtable the vtable to resolve
+	 * @return the equivalent vtable managed by this ClassTypeInfoManager
+	 */
 	Vtable resolve(ArchivedGnuVtable vtable);
 
+	/**
+	 * Gets the Type info at the specified address. If the TypeInfo is a ClassTypeInfo
+	 * it can be resolved if requested.
+	 * @param address the address of the TypeInfo
+	 * @param resolve true to resolve the TypeInfo if it happens to be a ClassTypeInfo
+	 * @return the TypeInfo at the address
+	 */
 	TypeInfo getTypeInfo(Address address, boolean resolve);
 
 }
