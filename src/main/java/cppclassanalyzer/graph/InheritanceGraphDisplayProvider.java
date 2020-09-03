32ae04b059eff4012458f35dfa2b317ab54516f5
astrelsky <ajs222@njit.edu>
2020-09-03 09:00:25 -0400

    Add unimplemented graph display provider

---
+++ src/main/java/cppclassanalyzer/graph/InheritanceGraphDisplayProvider.java
@@ -0,0 +1,11 @@
+package cppclassanalyzer.graph;
+
+import ghidra.graph.visualization.DefaultGraphDisplayProvider;
+
+public class InheritanceGraphDisplayProvider extends DefaultGraphDisplayProvider {
+
+	@Override
+	public String getName() {
+		return "Inheritance Graph Display (Not Implemented)";
+	}
+}
